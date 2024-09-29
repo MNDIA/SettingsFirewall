493d6e3311e036392e36f9f33bfb3ccbf7ebd270
Nyan_yay <YP@users.github.com>
2024-09-29 22:13:48 +0800

    Refactor build.gradle and App.kt files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://api.xposed.info' }
+        maven {
+            url = uri("https://api.xposed.info")
+        }
     }
 }
+
 rootProject.name = "SettingsFirewall"
-include ':app'
+include(":app")
